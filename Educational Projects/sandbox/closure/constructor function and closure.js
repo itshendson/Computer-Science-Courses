d7dd196273308e32653dfc1d97bbbc3bb3a42425
Hendson <hendsonszeto@live.com>
2022-04-11 21:58:02 -0700

    add special note to closure and arrow functions

--- Educational Projects/sandbox/closure/constructor function and closure.js
+++ Educational Projects/sandbox/closure/constructor function and closure.js
@@ -1,30 +1,36 @@
-// Counter = () => {
-//     let count = 0;
-//     this.incrementCounter = () => {
-//         count++;
-//         console.log(count)
-//     }
-//     this.decrementCounter = () => {
-//         count--;
-//         console.log(count)
-//     }
-// }
-
-// var counterA = new Counter();
-// counterA.incrementCounter();
-
 function Counter() {
-    var count = 0;
-    this.incrementCounter = function() {
+    let count = 0;
+    this.incrementCounter = () => {
         count++;
-        console.log(count);
+        console.log(count)
     }
-    this.decrementCounter = function() {
+    this.decrementCounter = () => {
         count--;
-        console.log(count);
+        console.log(count)
     }
 }
 
-var counter1 = new Counter();
-counter1.incrementCounter();
-counter1.decrementCounter();
+var counterA = new Counter();
+counterA.incrementCounter();
+counterA.incrementCounter();
+counterA.decrementCounter();
+
+// function Counter() {
+//     var count = 0;
+//     this.incrementCounter = function() {
+//         count++;
+//         console.log(count);
+//     }
+//     this.decrementCounter = function() {
+//         count--;
+//         console.log(count);
+//     }
+// }
+
+// var counter1 = new Counter();
+// counter1.incrementCounter();
+// counter1.decrementCounter();
+
+
+// Special Notes:
+// - Arrow functions CANNOT be used as constructors. They cannot be called with the new keyword and will throw an error indicating that the fuction is not a constructor. 
