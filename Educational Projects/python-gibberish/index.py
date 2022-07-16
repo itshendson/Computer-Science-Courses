f37c575b78d291fce1ca07de3d98b096b6316947
Hendson <hendsonszeto@live.com>
2022-07-15 21:06:50 -0700

    experimenting with python

---
+++ Educational Projects/python-gibberish/index.py
@@ -0,0 +1,16 @@
+# print("Hello World")
+# print("This is a test of your automated systems")
+
+from re import X
+
+
+x = str(10)
+y = 15
+
+# if x < y:
+#     print("Yes, 10 is less than 15")
+
+if x is str:
+    print("x is a string")
+else:
+    print("x is not a string")
